b2808e081225c4ed5d0c01427a3c74205e976a5c
oiRazor <rozariocrossley1996@gmail.com>
2017-02-25 15:30:56 -0500

    edit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var mapimg;
 
-var clat = 0;
-var clon = 0;
+var clat = 40.735657;
+var clon = -74.172363;
 
 var ww = 1024;
 var hh = 512;
@@ -18,20 +18,6 @@ function preload() {
   earthquakes = loadStrings('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
 
-function mercX(lon) {
-  lon = radians(lon);
-  var a = (256 / PI) * pow(2, zoom);
-  var b = lon + PI;
-  return a * b;
-}
-
-function mercY(lat) {
-  lat = radians(lat);
-  var a = (256 / PI) * pow(2, zoom);
-  var b = tan(PI / 4 + lat / 2);
-  var c = PI - log(b);
-  return a * c;
-}
 
 
 function setup() {
@@ -39,25 +25,5 @@ function setup() {
   translate(width / 2, height / 2);
   imageMode(CENTER);
   image(mapimg, 0, 0);
-
-  var cx = mercX(clon);
-  var cy = mercY(clat);
-
-  for (var i = 1; i < earthquakes.length; i++) {
-    var data = earthquakes[i].split(/,/);
-    //console.log(data);
-    var lat = data[1];
-    var lon = data[2];
-    var mag = data[4];
-    var x = mercX(lon) - cx;
-    var y = mercY(lat) - cy;
-    mag = pow(10, mag);
-    mag = sqrt(mag);
-    var magmax = sqrt(pow(10, 10));
-    var d = map(mag, 0, magmax, 0, 180);
-    stroke(255, 0, 255);
-    fill(255, 0, 255, 200);
-    ellipse(x, y, d, d);
-  }
 
 }
